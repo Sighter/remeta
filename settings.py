809694c59c34849a97b5e3a71f94edffa84f5bff
sighter <sighter@resource-dnb.de>
2010-12-05 19:44:45 +0100

    added settings file

---
+++ settings.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# settings.py -- Settings Module
+# @Author:      The Sighter (sighter@resource-dnb.de)
+# @License:     GPL
+# @Created:     2010-12-05.
+# @Revision:    0.1
+
+
+
+
+copymode = None
+
+pattern_user = None
+pattern_default = "%n %a - %t %k"
