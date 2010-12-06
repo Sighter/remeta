0400c3a2e69b1cec0bfeb1227bafdfff158069ae
sighter laptop <sighter@resource-dnb.de>
2010-12-06 15:56:05 +0100

    script now works with id3tags

--- remeta.py
+++ remeta.py
@@ -10,7 +10,10 @@ import getopt
 import sys
 import settings
 from helpers import ePrint
+from helpers import split_ld
 from os import path
+from os import rename
+import shutil
 from id3ed import id3_to_track
 import pattern
 
@@ -23,9 +26,15 @@ import pattern
 # **************************
 def print_help ():
 	sFktname = "print_help"
-	print("Synopsis: remeta file1 file2 ... [ -c ]")
+	print("Synopsis: remeta file1 file2 ... [ -c ] [ -p pattern ]")
 	print("  -c	| --copy     make a copy of original files before copying")
 	print("  -h	| --help     print this help")
+	print("  -p	| --pattern  specify pattern for renaming\n")
+	print("Keys for patterns:")
+	print("%a - Artist")
+	print("%t - Title")
+	print("%n - Track number")
+	print("%k - Harmonic key")
 # end of print_help
 
 
@@ -43,22 +52,24 @@ def main ():
 	try:
 		opts, args = getopt.getopt(sys.argv[1:], "hcp:", ["help", "copy", "pattern="])
 	except:
-		print(err) # will print something like "option -a not recognized"
-		usage()
+		print_help ()
 		sys.exit(2)
 
-	
 	# handle options
 	for o, a in opts:
 		if o in ("-h", "--help"):
 			print_help()
+			sys.exit(2)
 		elif o in ("-c", "--copy"):
 			settings.copymode = True
 		elif o in ("-p", "--pattern"):
+			settings.pattern_user = a
 				
 	# handle arguments
 	if len(args) == 0:
 		ePrint(1, sFktname, "No Files specified")
+	
+	print(args)
 	
 	# check, whether files have valid paths
 	for fn in args:
@@ -66,22 +77,41 @@ def main ():
 			ePrint(1, sFktname, "Path not found: {}".format(fn))
 			sys.exit(2)
 	
-	# 
-	
+
 	# enter main loop
 	for fn in args:
+
+		# fill pattern
+		if settings.pattern_user:
+			pat = pattern.pattern(settings.pattern_user)
+		else:
+			pat = pattern.pattern(settings.pattern_default)
 
 		# try to get information from tags
 		new_track = id3_to_track(fn)
 
-		# artist found ?
-		if new_track.artist != "":
+		suffix = split_ld(fn)
+
+		if new_track.is_empty() == False: 
+			pat.replace_artist(new_track.artist)
+			pat.replace_title(new_track.title)
+			pat.replace_tn(new_track.tn)
+			pat.replace_key(new_track.key)
+			final_name = "{}.{}".format(pat.get_result(), suffix)
+		else:
+			final_name = fn
+
+		ePrint(1, sFktname, "{} will be renamed to: {}".format(fn, final_name))
+
+		# rename or copy
+		if fn == final_name:
+			continue
+		else:
+			if settings.copymode:
+				shutil.copy(fn, final_name)
+			else:
+				rename(fn, final_name)
 			
-			
-			
-			
-		
-		
 
 # end of main
 
