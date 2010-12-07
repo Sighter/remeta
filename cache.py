197082229493b50f739eca5e187eee3f6ffd2654
sighter laptop <sighter@resource-dnb.de>
2010-12-07 14:11:41 +0100

    added cache module

---
+++ cache.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# cache.py -- Module to hold some cache lists
+# @Author:      The Sighter (sighter@resource-dnb.de)
+# @License:     GPL
+# @Created:     2010-12-07.
+# @Revision:    0.1
+
+
+rels_found = []
