38bb017a519b5dcbca902a3424d350f10274e565
sighter <sighter@resource-dnb.de>
2010-12-08 19:56:57 +0100

    added stagger lib

---
+++ stagger/__init__.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2009, Karoly Lorentey  <karoly@lorentey.hu>
+
+import stagger.frames
+import stagger.tags
+import stagger.id3
+import stagger.util
+
+from stagger.errors import *
+from stagger.frames import Frame, ErrorFrame, UnknownFrame, TextFrame, URLFrame
+from stagger.tags import read_tag, decode_tag, delete_tag, Tag22, Tag23, Tag24
+from stagger.id3v1 import Tag1
+
+version = (0, 3, 0)
+
+default_tag = Tag24
+
+stagger.util.python_version_check()
